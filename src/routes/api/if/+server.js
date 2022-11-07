b2c9bfc2e768f28a454d2354f19356da88f6cf46
Sam Littlefair <sam.littlefair@prismic.io>
2022-11-07 14:27:46 +0000

    Rebuild

--- src/routes/api/if/+server.js
+++ src/routes/api/if/+server.js
@@ -1,52 +1,90 @@
-import { error } from '@sveltejs/kit'
-import { Client, Environment } from 'square'
-import { env } from '$env/dynamic/private'
+import { error } from '@sveltejs/kit';
+import { Client, Environment } from 'square';
+import { env } from '$env/dynamic/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({}) {
-  const client = new Client({
-    accessToken: env.SQUARE_ACCESS_TOKEN,
-    types: 'ITEM',
-  })
+	const client = new Client({
+		accessToken: env.SQUARE_ACCESS_TOKEN
+	});
 
-  const { catalogApi } = client
+	const { catalogApi, checkoutApi } = client;
 
-  const response = await catalogApi.listCatalog()
+	const {
+		result: { objects: catalogObjects }
+	} = await catalogApi.listCatalog(undefined, 'ITEM');
 
-  const items = response.result.objects.filter(
-    (objects) => objects.type === 'ITEM',
-  )
+	console.log(
+		catalogObjects.map(
+			(object) => object.itemData.variations[0].itemVariationData.locationOverrides[0]
+		)
+	);
 
-  const results = items.map((item) => {
-    const {
-      id,
-      updatedAt: last_update,
-      itemData: {
-        name: title,
-        description,
-        imageIds: [image_url],
-      },
-    } = item
-    return {
-      id,
-      title,
-      description,
-      image_url,
-      last_update,
-      blob: {
-        title,
-        description,
-        image_url,
-      },
-    }
-  })
+	const objectIds = catalogObjects.map((object) => object.id);
 
-  const data = {
-    results_size: items.length,
-    results,
-  }
+	const {
+		result: { relatedObjects }
+	} = await catalogApi.batchRetrieveCatalogObjects({
+		objectIds,
+		includeRelatedObjects: true
+	});
 
-  const string = JSON.stringify(data)
+	const imageURLs = new Object();
 
-  return new Response(string)
+	relatedObjects.forEach((object) => {
+		if (object.type === 'IMAGE') {
+			Object.assign(imageURLs, {
+				[object.id]: object.imageData.url
+			});
+		}
+	});
+
+	const results = catalogObjects.map((item) => {
+		const {
+			id,
+			updatedAt,
+			itemData: {
+				name: title,
+				description,
+				imageIds: [image_id],
+				variations: [
+					{
+						itemVariationData: {
+							priceMoney: { amount, currency },
+							locationOverrides: [{ soldOut }]
+						}
+					}
+				]
+			}
+		} = item;
+
+		const last_update = new Date(updatedAt).getTime();
+		const image_url = imageURLs[image_id];
+		const price = Number(amount);
+
+		return {
+			id,
+			title,
+			description,
+			image_url,
+			last_update,
+			blob: {
+				title,
+				description,
+				image_url,
+				price,
+				currency,
+				soldOut
+			}
+		};
+	});
+
+	const data = {
+		results_size: catalogObjects.length,
+		results
+	};
+
+	const string = JSON.stringify(data);
+
+	return new Response(string);
 }
