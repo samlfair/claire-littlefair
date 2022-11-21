a64cd676edbf1a5149ca12d27230eb1bf96d8a99
Sam Littlefair <sam.littlefair@prismic.io>
2022-11-21 15:17:16 +0000

    Debug page transition issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-  import {SliceZone} from "@prismicio/svelte"
-  import { dev } from "$app/environment"
-  import components from "$lib/components/slices"
+  import { SliceZone } from '@prismicio/svelte'
+  import { dev } from '$app/environment'
+  import components from '$lib/components/slices'
 
   export let data
   const { document } = data
+  $: data, document
 </script>
 
 <SliceZone slices={document?.data?.body} {components} {dev} />
--- src/routes/[uid]/+page.svelte
+++ src/routes/[uid]/+page.svelte
@@ -1,10 +1,12 @@
 <script>
-  import {SliceZone} from "@prismicio/svelte"
-  import { dev } from "$app/environment"
-  import components from "$lib/components/slices"
+  import { SliceZone } from '@prismicio/svelte'
+  import { dev } from '$app/environment'
+  import components from '$lib/components/slices'
 
   export let data
   const { document } = data
 </script>
 
-<SliceZone slices={document?.data?.body} {components} {dev} />
+{#key document.uid}
+  <SliceZone slices={document?.data?.body} {components} {dev} />
+{/key}
