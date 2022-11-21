84e8b97cf4bc4ca3a19b9c06f0637dc31f4999c2
Sam Littlefair <sam.littlefair@prismic.io>
2022-11-21 17:10:44 +0000

    Refactor page route

--- src/routes/[[uid]]/+page.server.js
+++ src/routes/[[uid]]/+page.server.js
@@ -1,9 +1,10 @@
 import { createClient } from '$lib/prismicio'
 
 export async function load({ request, params }) {
+  console.log(params)
   const client = createClient({ request })
 
-  const document = await client.getByUID('page', params.uid)
+  const document = await client.getByUID('page', params.uid || 'homepage')
 
   if (document) {
     return { document }
