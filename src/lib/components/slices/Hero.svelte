40f34663dfade6dd34894462f64883c8d4688ec8
Sam Littlefair <sam.littlefair@prismic.io>
2022-11-28 09:51:59 +0000

    Refactor hero

--- src/lib/components/slices/Hero.svelte
+++ src/lib/components/slices/Hero.svelte
@@ -1,9 +1,82 @@
 <script>
+  import * as prismicH from '@prismicio/helpers'
+
   export let slice
+
+  let scrollY, innerHeight, el
+
+  const { text, image } = slice.primary
+  const { alt, dimensions } = image
+  const { width, height } = dimensions
+  const { src, srcset } = prismicH.asImageWidthSrcSet(image)
+
+  function getElementScrollCoefficient(scrollY, innerHeight, el) {
+    if (el) {
+      const { top, bottom, height } = el.getBoundingClientRect()
+      const elementScrollCoefficient =
+        (innerHeight - top) / (innerHeight + height)
+
+      switch (true) {
+        case elementScrollCoefficient < 0:
+          // Element is above the screen
+          return 0
+        case elementScrollCoefficient > 1:
+          // Element is below the screen
+          return 1
+        case top + scrollY < innerHeight:
+          // Element starts above the fold
+          return scrollY / (bottom + innerHeight)
+        default:
+          // Element starts below the fold
+          return elementScrollCoefficient
+      }
+    }
+    // Element doesn't exist
+    return 0
+  }
 </script>
 
+<svelte:window bind:scrollY bind:innerHeight />
+
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+<hr />
+
 <section class="hero">
-  <img src={slice.primary.image.url} alt="" />
+  <div class="image" bind:this={el}>
+    <img
+      style:--scroll-coefficient={getElementScrollCoefficient(
+        scrollY,
+        innerHeight,
+        el,
+      )}
+      {src}
+      {srcset}
+      {alt}
+      {width}
+      {height}
+    />
+  </div>
   <div class="bound">
     <div class="text">
       <p>{slice.primary.text}</p>
@@ -16,10 +89,16 @@
     position: relative;
   }
 
+  .image {
+    width: 100vw;
+    overflow: hidden;
+  }
   img {
     min-width: 100%;
-    min-height: 50vh;
+    object-fit: cover;
+    min-height: 40vh;
     display: block;
+    filter: hue-rotate(calc(var(--scroll-coefficient) * 180deg));
   }
 
   .text {
