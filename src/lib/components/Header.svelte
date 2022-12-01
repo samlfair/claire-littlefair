871b4fe30efc180016e995752b93dc4e5f09ed10
Sam Littlefair <sam.littlefair@prismic.io>
2022-12-01 10:06:59 +0000

    Make nav responsive

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -14,6 +14,7 @@
 </script>
 
 <header>
+  <input type="checkbox" id="burger" />
   {#each menu as item}
     <li>
       <a prefetch href={prismicH.asLink(item.link)}>
@@ -21,7 +22,10 @@
       </a>
     </li>
   {/each}
-  <li class="title"><a href="/">{site_title}</a></li>
+  <li class="title">
+    <a href="/">{site_title}</a>
+    <label class="burger" for="burger" />
+  </li>
   {#each menu_right as item}
     <li>
       <a href={prismicH.asLink(item.link)}>
@@ -33,7 +37,6 @@
 
 <style>
   header {
-    border: 1px solid black;
     border-right: none;
     display: flex;
     background: var(--lightblue);
@@ -42,21 +45,74 @@
   li {
     list-style: none;
     border-collapse: collapse;
-    border-right: 1px solid black;
+    border-bottom: 1px solid black;
     flex-grow: 0;
     text-align: center;
     padding: 0.7rem 1.1rem;
     font-size: 0.9rem;
   }
 
+  li:not(:last-child) {
+    border-right: 1px solid black;
+  }
+
   li.title {
     flex-grow: 1;
     font-family: Americana;
     font-size: 1.3em;
+    white-space: pre;
   }
 
   a {
     color: black;
     text-decoration: none;
+  }
+
+  input#burger,
+  label.burger {
+    display: none;
+  }
+
+  @media only screen and (max-width: 900px) {
+    header {
+      flex-direction: column;
+      align-items: stretch;
+    }
+
+    li:not(.title) {
+      display: none;
+    }
+
+    .title {
+      order: -1;
+      display: flex;
+    }
+
+    .title a {
+      flex-grow: 1;
+    }
+
+    label.burger {
+      display: initial;
+      width: 0px;
+      position: relative;
+    }
+
+    label.burger:after {
+      position: absolute;
+      right: 0px;
+    }
+
+    #burger:not(:checked) ~ li.title label.burger:after {
+      content: '☰';
+    }
+    
+    #burger:checked ~ li.title label.burger:after {
+      content: '×';
+    }
+
+    #burger:checked ~ li:not(.title) {
+      display: initial;
+    }
   }
 </style>
