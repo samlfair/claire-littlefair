39aea0bc2829a9ea9297031c4546ff0803217fe1
Sam Littlefair <sam.littlefair@prismic.io>
2022-12-13 19:37:55 +0000

    Add image flip

--- src/lib/slices/Products.svelte
+++ src/lib/slices/Products.svelte
@@ -1,38 +1,12 @@
 <script>
-  import * as prismicH from '@prismicio/helpers'
-  import { SoldOut } from '$lib/components'
+  import { Product } from '$lib/components'
 
   export let slice
-
-  const { items } = slice
 </script>
 
 <section class="bound">
-  {#each items as { product }}
-    {@const { images, square_item } = product.data}
-    {@const { price, currency, description, soldOut } = square_item}
-    {@const { alt, dimensions } = images[0].image}
-    {@const { height, width } = dimensions}
-    {@const { src, srcset } = prismicH.asImageWidthSrcSet(images[0].image)}
-    {@const formattedPrice = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency,
-    }).format(price / 100)}
-    <a href={prismicH.asLink(product)}>
-      <div class="image" {width} {height}>
-        <img {src} {alt} />
-      </div>
-      <h2>{square_item.title}</h2>
-      <p class="details">
-        <span class="price">
-          £{(price / 100).toFixed(2)}
-          {currency}
-        </span>
-        {#if soldOut}
-          <SoldOut />
-        {/if}
-      </p>
-    </a>
+  {#each slice.items as { product }}
+    <Product {product} />
   {/each}
 </section>
 
@@ -43,35 +17,5 @@
     gap: 1rem;
     margin-top: 3rem;
     margin-bottom: 3rem;
-  }
-
-  a {
-    text-decoration: none;
-    color: initial;
-  }
-
-  .image {
-    width: 100%;
-    height: auto;
-    background: radial-gradient(circle, white 0%, #eee 100%);
-  }
-
-  img {
-    width: 100%;
-    height: auto;
-    aspect-ratio: 1;
-    mix-blend-mode: darken;
-    margin-bottom: 1rem;
-    display: block;
-    object-fit: cover;
-  }
-
-  h2 {
-    font-size: 1rem;
-    margin-bottom: 0.5rem;
-  }
-
-  p {
-    font-size: 0.8rem;
   }
 </style>
